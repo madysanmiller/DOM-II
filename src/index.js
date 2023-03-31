b8ed47bc3e5cf1d4bbdced84cfdec1c657c27d08
Madysan Miller <madysanmiller2020@gmail.com>
2023-03-31 12:55:29 -0400

    attempt number 6

--- src/index.js
+++ src/index.js
@@ -57,46 +57,49 @@ import './less/index.less'
 //     alert("window loading alert")
 // })
 
-window.onload = function (event) {
-    console.log(`event ${event.type} fired! Ready To Go!`)
+window.onload = function (evt){
+    console.log(`event ${evt.type} has fired. It has finished`);
     const heading = document.querySelector('h1')
-    heading.textContent= 'READY TO GO!'
-    console.log(heading)
-}
-
-window.addEventListener('copy', () => {
-    navigator.clipboard.readText()
-    .then(text => {
-        heading.textContent += text
+    heading.textContent = 'Lets go'
+    
+    
+    
+    window.addEventListener('copy', () =>{
+        navigator.clipboard.readText()
+        .then(text =>{
+            heading.textContent += text
+        })
+     })
+     window.addEventListener('click', ()=>{
+        const subHead = document.querySelector('h2')
+        subHead.classList.add('off')
+        subHead.style.fontFamily='italic'
+        console.log(subHead)
+     })
+     window.addEventListener('dblclick', ()=>{
+        const subHead = document.querySelector('h2')
+        subHead.classList.remove('off')
+        subHead.style.fontFamily = 'sans-serif'
+     })
+     window.addEventListener('keydown', evt => {
+        if(evt.key === 'Escape'){
+            document.body.innerHTML = 'YOU HAVE CALLED ORDER 66'
+        }
+     })
+    //  Annoying one
+    document.body.addEventListener('mousemove', evt => {
+        const {clientx, xlienty} = evt
+        // console.log(evt)
     })
-})
 
-document.body.addEventListener('click', event => {
-    event.target.classList.toggle('mirror')
-})
-
-document.body.addEventListener('dblclick', event => {
-    event.target.innerHTML = ''
-})
-
-window.addEventListener('keydown', event => {
-    if (event.key == 6) {
-        document.body.innerHTML = '<h1>YOU RAN ORDER 66</h1>'
+    const destinations = document.querySelectorAll('.destination')
+    console.log(destinations)
+    for(let destination of destinations){
+        destination.addEventListener('mouseenter', evt =>{
+            destination.style.fontWeight = 'bold'
+        })
+        destination.addEventListener('mouseleave', evt =>{
+            destination.style.fontWeight = 'intial'
+        })
     }
-})
-
-document.body.addEventListener('mousemove', event => {
-    const { clientX, clientY } = event
-})
-
-const destinations = document.querySelectorAll('.destination')
-for (let destination of dsestinations) {
-    destination.addEventListener('mouseenter', () => {
-        destination.style.fontweight = 'bold'
-    })
-    destination.addEventListener('mouseleave', () => {
-        setTimeout(() => {
-
-        }, 500)
-    })
 }
