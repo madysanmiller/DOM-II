8b4a1eb371f139bb81aaa20664c3dc889e117335
Madysan Miller <madysanmiller2020@gmail.com>
2023-03-31 11:54:26 -0400

    finish tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './less/index.less'
 
 // Your code goes here!
+
+const headerNav = document.querySelector(".nav")
+const navLInk = document.querySelectorAll(".nav a")
+
+headerNav.addEventListener("mouseover", (event)=> {
+    event.target.style.color = "blue"
+})
+
+
+navLInk.forEach(link => {
+    link.addEventListener("click", (e)=> {
+        e.preventDefault()
+    })
+})
+
+
+document.addEventListener("dblclick", (e)=> {
+    e.target.style.color = "pink"
+    })
+
+
+document.addEventListener("keydown", (e)=> {
+alert(e.key)
+})
+
+
+document.addEventListener("wheel", ()=>{
+navLInk.forEach(link => link.style.color = "green")
+})
+
+
+window.addEventListener("resize", ()=> {
+    alert("resizing")
+})
+
+
+document.addEventListener("select", (e)=> {
+e.target.style.color = "purple"
+})
+
+
+window.addEventListener("load", ()=> {
+    alert("window loading alert")
+})
+
+
+document.addEventListener("scroll", ()=>{
+    navLInk[0].style.color = "red"  
+})
